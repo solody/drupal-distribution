8d025b3b993a20e0b4598868e7aa4f11be467f6b
kentkent <kent@solody.com>
2018-04-27 14:50:29 +0800

    添加分销商任务菜单

---
+++ src/Controller/TaskRedirectController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Drupal\distribution\Controller;
+
+use Drupal\distribution\DistributionManager;
+use Drupal\distribution\Entity\Distributor;
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Class TaskRedirectController.
+ */
+class TaskRedirectController extends ControllerBase
+{
+
+    /**
+     * Owner.
+     *
+     * @param Distributor $distribution_distributor
+     * @return string
+     *   Return Hello string.
+     */
+    public function owner(Distributor $distribution_distributor)
+    {
+        $user = $distribution_distributor->getOwner();
+        return $this->redirect('entity.user.edit_form', ['user' => $user->id()]);
+    }
+
+
+    public function financeAccount(Distributor $distribution_distributor)
+    {
+        $query = \Drupal::entityQuery('finance_account');
+        $query->condition('user_id', $distribution_distributor->getOwnerId());
+        $query->condition('type', DistributionManager::FINANCE_ACCOUNT_TYPE);
+        $entity_ids = $query->execute();
+
+        if (empty($entity_ids)) return [
+            '#markup' => '<h4>找不到记账账户。</h4>'
+        ];
+
+        return $this->redirect('entity.finance_account.canonical', ['finance_account' => array_pop($entity_ids)]);
+    }
+}
