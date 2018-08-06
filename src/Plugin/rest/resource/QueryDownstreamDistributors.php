9221e32986326a00593c5a7867d1626f55a7b210
kentkent <kent@solody.com>
2018-08-06 18:58:15 +0800

    add api

---
+++ src/Plugin/rest/resource/QueryDownstreamDistributors.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Drupal\distribution\Plugin\rest\resource;
+
+use Drupal\Core\Session\AccountProxyInterface;
+use Drupal\rest\ModifiedResourceResponse;
+use Drupal\rest\Plugin\ResourceBase;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+
+/**
+ * Provides a resource to get view modes by entity and bundle.
+ *
+ * @RestResource(
+ *   id = "distribution_query_downstream_distributors",
+ *   label = @Translation("Query downstream distributors"),
+ *   uri_paths = {
+ *     "create" = "/api/rest/distribution/query-downstream-distributors"
+ *   }
+ * )
+ */
+class QueryDownstreamDistributors extends ResourceBase {
+
+  /**
+   * A current user instance.
+   *
+   * @var \Drupal\Core\Session\AccountProxyInterface
+   */
+  protected $currentUser;
+
+  /**
+   * Constructs a new QueryDownstreamDistributors object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param array $serializer_formats
+   *   The available serialization formats.
+   * @param \Psr\Log\LoggerInterface $logger
+   *   A logger instance.
+   * @param \Drupal\Core\Session\AccountProxyInterface $current_user
+   *   A current user instance.
+   */
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    array $serializer_formats,
+    LoggerInterface $logger,
+    AccountProxyInterface $current_user) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $serializer_formats, $logger);
+
+    $this->currentUser = $current_user;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->getParameter('serializer.formats'),
+      $container->get('logger.factory')->get('distribution'),
+      $container->get('current_user')
+    );
+  }
+
+  /**
+   * Responds to POST requests.
+   *
+   * @param $data
+   * @return \Drupal\rest\ModifiedResourceResponse
+   *   The HTTP response object.
+   *
+   */
+  public function post($data) {
+
+    // You must to implement the logic of your REST Resource here.
+    // Use current user after pass authentication to validate access.
+    if (!$this->currentUser->hasPermission('access content')) {
+      throw new AccessDeniedHttpException();
+    }
+
+    if (!isset($data['distributor'])) {
+      throw new BadRequestHttpException('distributor is required.');
+    }
+
+    $query = \Drupal::entityQuery('distribution_distributor');
+
+    return new ModifiedResourceResponse($data, 200);
+  }
+
+}
